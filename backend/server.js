1f3ae033544e6ca61301e56f4e2118e8c7b75644
Zakaria Moshi <zkemoshi@Mac.Home>
2025-08-25 01:52:46 +0100

    routefix

--- backend/server.js
+++ backend/server.js
@@ -52,9 +52,9 @@ if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-  );
+  // app.get("*", (req, res) =>
+  //   res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  // );
 } else {
   console.log('devmode')
   app.get("/", (req, res) => {
